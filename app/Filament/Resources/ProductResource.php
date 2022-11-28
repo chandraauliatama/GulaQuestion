955cdef25b91fba41725658e5838ebab9fa843b6
chandraauliatama <chandraaulia4@gmail.com>
2022-11-28 17:31:47 +0700

    Create product migration, filament resource, model Fix redirect in admin page

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    protected static function getNavigationBadge(): ?string
+    {
+        return 1;
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('brand')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('category')
+                    ->options(['makanan' => 'Makanan', 'minuman' => 'Minuman'])
+                    ->required(),
+                Forms\Components\TextInput::make('BPOM ID')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('weight')
+                    ->required(),
+                Forms\Components\TextInput::make('sugar')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('brand')->searchable()->sortable(),
+                Tables\Columns\BadgeColumn::make('category')
+                    ->enum(['makanan' => 'Makanan', 'minuman' => 'Minuman'])
+                    ->colors(['danger' => 'makanan', 'success' => 'minuman'])
+                    ->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('BPOM ID'),
+                Tables\Columns\TextColumn::make('weight'),
+                Tables\Columns\TextColumn::make('sugar'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ database/migrations/2022_11_28_022228_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Filament\Forms\Components\Tabs\Tab;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('brand');
+            $table->enum('category', ['makanan', 'minuman']);
+            $table->string('BPOM ID');
+            $table->float('weight');
+            $table->float('sugar'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
